51d0d6576f21a4a0295ecfe1cc0cd9c14c13b070
ali4z <howdy1@gmail.com>
2012-05-13 04:23:39 +0200

    TileEntityRedstoneWireless to Common. - Added TileEntityRedstoneWirelessInjector.doUpdateEntity for uncommon SSP and Server code.

---
+++ wifiserver/net/minecraft/src/wifi/TileEntityRedstoneWirelessInjector.java
@@ -0,0 +1,9 @@
+package net.minecraft.src.wifi;
+
+import net.minecraft.src.World;
+
+public class TileEntityRedstoneWirelessInjector {
+	public static boolean doUpdateEntity(World world) {
+		return ( world != null );
+	}
+}
